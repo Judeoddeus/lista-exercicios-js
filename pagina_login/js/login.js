872bd69d8f0d6615c57fc76de12ddaa9f3342cb2
Judeoddeus <64862169+Judeoddeus@users.noreply.github.com>
2021-03-17 22:35:00 -0300

    atualização do repositório

---
+++ pagina_login/js/login.js
@@ -0,0 +1,14 @@
+let btnSignin = document.querySelector("#signin");
+let btnSignup = document.querySelector("#signup");
+
+btnSignup.animate
+
+let body = document.querySelector("body");
+
+btnSignin.addEventListener("click", function(){
+    body.className = "sign-in-js";
+});
+
+btnSignup.addEventListener("click", function(){
+    body.className = "sing-up-js";
+});
